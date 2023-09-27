ccf80b94b9d1e41740b65b780a1fd985b2a71616
albertogomesdasilva <albertogomesdasilva@hotmail.com>
2023-09-27 12:56:29 -0300

    roles admin

---
+++ database/seeds/0001RoleSeeder.js
@@ -0,0 +1,43 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| RoleSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+// /** @type {import('@adonisjs/lucid/src/Factory')} */
+// const Factory = use('Factory')
+
+
+const Role = use('Role')
+
+class RoleSeeder {
+  async run () {
+    //cria o admin
+    await Role.create({
+      name: 'Admin',
+      slug: 'admin',
+      description: 'Administrador do sistema'
+    })
+    // cria o cargo de gerente
+    await Role.create({
+      name: 'Manager',
+      slug: 'manager',
+      description: 'Gerente da loja'
+    })
+    // cria o cargo de cliente
+    await Role.create({
+      name: 'Client',
+      slug: 'client',
+      description: 'Cliente da loja'
+    })
+
+  }
+}
+
+module.exports = RoleSeeder
