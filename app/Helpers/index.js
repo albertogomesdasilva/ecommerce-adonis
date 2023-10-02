a2f28a234d19870b8f2e3f19bc73c7276039f96d
albertogomesdasilva <albertogomesdasilva@hotmail.com>
2023-10-02 14:33:05 -0300

    criação de Helpers

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-
-
 const crypto = use('crypto')
 const Helpers = use('Helpers')
 
@@ -17,12 +15,73 @@ const str_random = async (length = 40) => {
     let len = string.length
 
     if(len < length) {
-        let size = lenght - len
+        let size = length - len
         let bytes = await crypto.randomBytes(size)
-        let buffer =  Buffer.from(bytes) // Depreciado
-        string += buffer.toString('base64').replace(/[^a-zA-Z0-9]/g, '').substr(0, size)
+        let buffer =  Buffer.from(bytes)
+        string += buffer
+            .toString('base64')
+            .replace(/[^a-zA-Z0-9]/g, '')
+            .substr(0, size)
     }
     return string
+}
+
+/**
+ * Move um único arquivo para o caminho especificado, se nenhum caminho for especificado
+ * então 'public/uploads' será utilizado
+* @param { FileJar } file - arquivo a ser gerenciado
+* @param { string } path o caminho para onde o arquivo deverá ser movido
+ */ 
+
+const manage_single_upload = async (file, path = null ) => {
+    path = path ? path : Helpers.publicPath('uploadas')
+
+// Gera um nome aleatório
+const random_name = await str_random(30)
+let filename = `${new Date().getTime()} - ${random_name}.${file.subtype}`
+
+// Renomeia o arquivo e move para o path
+await file.move(path, {
+    name: filename
+})
+
+return file
+
+}
+
+
+/**
+ *  Move múltiplos arquivos para o caminho especificado, se nenhum for especificado
+ * então 'public/uploads' será utilizado.
+ * @param { fileJar } fileJar
+ * @param { string } path
+ * @return  { Object }
+*/
+
+const manage_multiple_uploads = async (fileJar, path = null) => {
+    path = path ?  path: Helpers.publicPath('uploads')
+    let successes = [],
+    errors = [] 
+
+    await Promise.all(fileJar.files.map(async file => {
+        let random_name = await str_random(30)
+        let filename = `${new Date().getTime()} - ${random_name}.${file.subtype}`
+
+        // Move o arquivo
+        await file.move(patch, {
+            name: filename
+        })
+
+        // Verificar se moveu o arquivo
+        if(file.moved()) {
+            successes.push(file)
+        } else {
+            errors.push(file.error())
+        }
+    })
+    )
+
+    return { successes: errors }
 }
 
 
